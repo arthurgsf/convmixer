5dff9147c4a43ec814b38a96dc445e5d4acca92d
arthurgsf <arthurgsf@nca.ufma.br>
2023-06-02 10:51:50 -0300

    patcher added

--- convmixer.py
+++ convmixer.py
@@ -92,7 +92,6 @@ class ConvMixer(tf.keras.layers.Layer):
             "strides"               :   self.strides,
             "data_format"           :   self.data_format,
             "activation_name"       :   self.activation,
-            "use_bias"              :   self.use_bias,
             "kernel_initializer"    :   self.kernel_initializer,
             "bias_initializer"      :   self.bias_initializer,
             "kernel_regularizer"    :   self.kernel_regularizer,
@@ -104,46 +103,77 @@ class ConvMixer(tf.keras.layers.Layer):
         base_config = super(ConvMixer, self).get_config()
         return dict(list(base_config.items()) + list(config.items()))
     
+class Patcher(tf.keras.layers.Layer):
+    def __init__(self,
+                 filters,
+                 patch_size:Tuple[int, int],
+                 activation="relu",
+                 data_format=None,
+                 kernel_initializer="glorot_uniform",
+                 bias_initializer="zeros", 
+                 kernel_regularizer=None,
+                 bias_regularizer=None, 
+                 activity_regularizer=None, 
+                 kernel_constraint=None,
+                 bias_constraint=None,
+                 **kwargs):
+        super(Patcher, self).__init__(**kwargs)
+        
+        self.filters                =   filters
+        self.patch_size            =   patch_size
+        self.activation_name        =   activation
+        self.data_format            =   data_format
+        self.kernel_initializer     =   kernel_initializer
+        self.bias_initializer       =   bias_initializer
+        self.kernel_regularizer     =   kernel_regularizer
+        self.bias_regularizer       =   bias_regularizer
+        self.activity_regularizer   =   activity_regularizer
+        self.kernel_constraint      =   kernel_constraint
+        self.bias_constraint        =   bias_constraint
 
-if __name__ == "__main__":
-    def build_model(n_classes = 10, depth = 3):
-        inputs = tf.keras.layers.Input(shape=(28, 28, 1))
-        t = inputs
-        for i in range(depth):
-            t = ConvMixer(256, (3, 3), (1, 1), "gelu")(t)
-        final_conv = tf.keras.layers.Conv2D(n_classes, 
-                                            kernel_size=1, 
-                                            strides=1, 
-                                            padding="same",
-                                            activation="relu")(t)
-        outputs = tf.keras.layers.GlobalAveragePooling2D()(final_conv)
-        outputs = tf.keras.layers.Dense(n_classes, "softmax")(outputs)
-        model = tf.keras.Model(inputs, outputs)
-        return model
-
-    def get_datasets():
-        import tensorflow_datasets as tfds
-        train_dataset = tfds.load("mnist", 
-            split="train", 
-            batch_size=32, 
-            shuffle_files=True,
-            as_supervised=True)
-        val_dataset = tfds.load("mnist", 
-            split="test", 
-            batch_size=32, 
-            shuffle_files=True,
-            as_supervised=True)
-        # test_dataset = tfds.load("mnist", split="test", batch_size=32, shuffle_files=True)
-        return train_dataset, val_dataset
+        self.conv = None
+        self.activation = None
     
-    model = build_model(
-        n_classes=10,
-        depth = 3)
-    model.compile("adam", "sparse_categorical_crossentropy", metrics=["acc"])
-    
-    train_dataset, val_dataset = get_datasets()
+    def build(self, input_shape):
+        self.conv = tf.keras.layers.Conv2D(
+            filters                 =   self.filters,
+            kernel_size             =   self.patch_size,
+            strides                 =   self.patch_size,
+            data_format             =   self.data_format,
+            kernel_initializer      =   self.kernel_initializer,
+            bias_initializer        =   self.bias_initializer,
+            kernel_regularizer      =   self.kernel_regularizer,
+            bias_regularizer        =   self.bias_regularizer,
+            activity_regularizer    =   self.activity_regularizer,
+            kernel_constraint       =   self.kernel_constraint,
+            bias_constraint         =   self.bias_constraint,
+            name                    =   "patcher_conv"
+        )
+        
+        self.activation = tf.keras.layers.Activation(self.activation_name)
 
-    model.fit(
-        train_dataset,
-        validation_data = val_dataset, 
-        epochs = 10)
+    def call(self, x, training=None):
+        x = self.conv(x)
+        x = self.activation(x)
+        return x
+
+    def compute_output_shape(self, input_shape):
+        return (input_shape[0], input_shape[1], self.filters)
+
+    def get_config(self):
+        config = {
+            "filters"               :   self.filters,
+            "patch_size"           :   self.patch_size,
+            "strides"               :   self.strides,
+            "data_format"           :   self.data_format,
+            "activation_name"       :   self.activation,
+            "kernel_initializer"    :   self.kernel_initializer,
+            "bias_initializer"      :   self.bias_initializer,
+            "kernel_regularizer"    :   self.kernel_regularizer,
+            "bias_regularizer"      :   self.bias_regularizer,
+            "activity_regularizer"  :   self.activity_regularizer,
+            "kernel_constraint"     :   self.kernel_constraint,
+            "bias_constraint"       :   self.bias_constraint,
+        }
+        base_config = super(Patcher, self).get_config()
+        return dict(list(base_config.items()) + list(config.items()))
